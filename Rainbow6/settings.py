52e7c9412ec9d178e7fa40f3ea0a755fd3cc4d90
Mrcrabus <bodenchyck97@mail.ru>
2020-02-25 17:28:09 +0300

    Finally change.

--- Rainbow6/settings.py
+++ Rainbow6/settings.py
@@ -120,6 +120,13 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')  # connect BASE_DIR with pictures
 MEDIA_URL = '/pictures/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'svonktv@gmail.com'
+EMAIL_HOST_PASSWORD = 'sdas'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'  # Open after u log in
--- Rainbow6/urls.py
+++ Rainbow6/urls.py
@@ -13,18 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from users import views as userViews
-from django.contrib.auth import views as authViews
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth import views as authViews
+from django.urls import path, include
+
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/', userViews.register, name="reg"),
     path('profile/', userViews.profile, name="profile"),
     path('user/', authViews.LoginView.as_view(template_name='users/user.html'), name='auth'),
+    path('pass-reset/', authViews.PasswordResetView.as_view(template_name='users/pass_reset.html'), name='pass-reset'),
+    path('password_reset_confirm/<uidb64>/<token>',
+         authViews.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password_reset_complete/<uidb64>/<token>',
+         authViews.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+    path('password_reset_done/done/',
+         authViews.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
     path('', include('blog.urls'))
 ]
