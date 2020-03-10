d8ebcbd47f76871fd5909a712d5dc1f7667bf68c
Mrcrabus <bodenchyck97@mail.ru>
2020-03-10 16:26:51 +0300

    Add article doing

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.ShowNewsView.as_view(), name='blog-home'),
+    path('news/add/', views.CreateNewsView.as_view(), name='news-add'),
     path('news/<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
     path('news/<str:username>/', views.UserAllNewsView.as_view(), name='user-news'),
 
     path('news/<int:pk>/update/', views.UpdateNewsView.as_view(), name='news-update'),
     path('news/<int:pk>/delete/', views.DeleteNewsView.as_view(), name='news-delete'),
-    path('news/add/', views.CreateNewsView.as_view(), name='news-add'),
+
     path('block/', views.block, name='blog-block'),
 ]
